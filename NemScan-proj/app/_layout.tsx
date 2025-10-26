bdd33f005a75a5301ceef060ce855cb6a5bad229
Mathias Foght <mathiasfoght98@gmail.com>
2025-10-26 02:26:43 +0200

    scan product by customer - double call to getProductCustomer fix

--- NemScan-proj/app/_layout.tsx
+++ NemScan-proj/app/_layout.tsx
@@ -1,16 +1,17 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "@/src/contexts/authContext";
+import { ProductProvider } from "@/src/contexts/productContext";
 
 export default function RootLayout() {
     return (
         <AuthProvider>
-            <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="index" />
-                <Stack.Screen name="loginScreen" options={{ animation: "slide_from_right"}} />
-                <Stack.Screen name="(tabs)" options={{ animation: "none", gestureEnabled: false }} />
-            </Stack>
+            <ProductProvider>
+                <Stack screenOptions={{ headerShown: false }}>
+                    <Stack.Screen name="index" />
+                    <Stack.Screen name="loginScreen" options={{ animation: "slide_from_right" }} />
+                    <Stack.Screen name="(tabs)" options={{ animation: "none", gestureEnabled: false }} />
+                </Stack>
+            </ProductProvider>
         </AuthProvider>
     );
 }
-
-
